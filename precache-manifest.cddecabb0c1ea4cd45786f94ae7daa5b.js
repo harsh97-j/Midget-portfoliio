583eb56cfe76a7cbd04e93488e659865ae9a4795
harsh97-j <harshkamle.97@gmail.com>
2020-05-14 19:41:52 +0500

    Updates

---
+++ precache-manifest.cddecabb0c1ea4cd45786f94ae7daa5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cef651aae5c1708ca9b1f14b221bd98",
+    "url": "/Midget-portfoliio/index.html"
+  },
+  {
+    "revision": "96b5266cdbed1d4aead4",
+    "url": "/Midget-portfoliio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "3963dbc0cf29d1e07ea2",
+    "url": "/Midget-portfoliio/static/css/main.a469c339.chunk.css"
+  },
+  {
+    "revision": "96b5266cdbed1d4aead4",
+    "url": "/Midget-portfoliio/static/js/2.8318ad4c.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/Midget-portfoliio/static/js/2.8318ad4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3963dbc0cf29d1e07ea2",
+    "url": "/Midget-portfoliio/static/js/main.63ff9901.chunk.js"
+  },
+  {
+    "revision": "cca1b57fb3400f17c19d",
+    "url": "/Midget-portfoliio/static/js/runtime-main.c9c90db9.js"
+  }
+]);
